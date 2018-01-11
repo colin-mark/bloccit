6e9c13c63475c8f604056e5f89a7b825a6c1bea3
colin_mark <colin.alcorn@gmail.com>
2018-01-10 21:19:24 -0600

    Assignment 19: started

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'question/index'
+
+  get 'question/show'
+
+  get 'question/new'
+
+  get 'question/edit'
+
   resources :posts
 
   get 'about' => 'welcome#about'
