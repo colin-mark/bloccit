5829b0158785994dded61224c9232cefa8f67ca7
colin_mark <colin.alcorn@gmail.com>
2018-02-10 10:18:03 -0600

    Checkpoint 27 Rails Authorization: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,10 +38,18 @@ posts = Post.all
   )
 end
 
-#Create specific user
-user = User.first
-user.update_attributes!(
-  email: 'colin.alcorn@gmail.com',
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 
