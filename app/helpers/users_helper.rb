3314353a490299df5c973956eaa1728f34fde0e1
colin_mark <colin.alcorn@gmail.com>
2018-03-21 21:26:20 -0500

    assignment 31: progress

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,38 @@
 module UsersHelper
+  def posts_header(user)
+    if user.posts.length > 0
+      "<h2> Posts </h2>"
+    else
+      "<p> #{user.name} has not submitted any posts yet. </p>"
+    end
+  end
+
+  def user_posts(user)
+      render user.posts
+  end
+
+  def comments_header(user)
+    if user.comments.length > 0
+      "<h2> Comments </h2>"
+    else
+      "<p> #{user.name} has not submitted any comments yet. </p>"
+    end
+  end
+
+  def user_comments(user)
+    render user.comments
+  end
+
+  def favorites_header(user)
+    favorites = Favorite.where(user: user)
+    if favorites.length > 0
+      "<h2> Favorites </h2>"
+    else
+      "<p> #{user.name} has not favorited any posts yet. </p>"
+    end
+  end
+
+  def user_favorites(user)
+    user.favorites.collect(&:post)
+  end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,15 +1,16 @@
-# require 'rails_helper'
-#
-# # Specs in this file have access to a helper object that includes
-# # the UsersHelper. For example:
-# #
-# # describe UsersHelper do
-# #   describe "string concat" do
-# #     it "concats two strings with spaces" do
-# #       expect(helper.concat_strings("this","that")).to eq("this that")
-# #     end
-# #   end
-# # end
-# RSpec.describe UsersHelper, type: :helper do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe UsersHelper, type: :helper do
+  let(:user) { create(:user) }
+
+  describe "#user_favorites(user)" do
+
+    it "returns 'nil' if the user has not favorited any posts" do
+      expect(user_favorites(user)).to be_nil
+    end
+
+    it "returns list of favorited posts" do
+      expect(user_favorites(user)).to eq(user.favorites)
+    end
+  end
+end
